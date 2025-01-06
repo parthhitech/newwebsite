cefe462c046c7c57057fe5d2502e59b5279b1020
pt-tp <parthtomar1019@gmail.com>
2025-01-06 14:59:26 +0530

    updated somethings

--- script/reviewslider.js
+++ script/reviewslider.js
@@ -1,24 +1,25 @@
-$(document).ready(function () {
-    $('#customers-testimonials').owlCarousel({
+jQuery(document).ready(function($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
       loop: true,
       center: true,
       items: 3,
       margin: 0,
       autoplay: true,
-      dots: true,
+      dots:true,
       autoplayTimeout: 8500,
       smartSpeed: 450,
       responsive: {
         0: {
-          items: 1,
+          items: 1
         },
         768: {
-          items: 2,
+          items: 2
         },
         1170: {
-          items: 3,
-        },
-      },
-    });
+          items: 3
+        }
+      }
   });
-
+});
